7b3e8b1ad0af5745c17737b3723e1d71405f3fe8
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-11-16 22:43:14 +0200

    Updates

--- src/utils/getFirstSeenEpisodeId.js
+++
@@ -1,5 +0,0 @@
-export const getFirstSeenEpisodeId = (character) => {
-  const epUrlParts = character.episode[0].split('/');
-
-  return +epUrlParts[epUrlParts.length - 1];
-};
--- src/utils/types/FilterKey.ts
+++
@@ -1,1 +0,0 @@
-export type FilterKey = 'character' | 'location' | 'episodes';
